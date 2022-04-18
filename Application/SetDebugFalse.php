2642ead309dc9f84dd1552d8e47df41251131983
thomasTMmagnum <54550535+thomastrringel@users.noreply.github.com>
2022-04-18 16:13:43 +0200

    Add files via upload

---
+++ Application/SetDebugFalse.php
@@ -0,0 +1,5 @@
+<?php
+    // setze Cookie debug=FALSE
+    setcookie("debug", "FALSE", time()+3600, "/mb/", "");
+
+?>
---
+++ Application/SetDebugTrue.php
@@ -0,0 +1,7 @@
+<?php
+    // setze Cookie debug=true
+    setcookie("debug", "TRUE", time()+3600, "/mb/", "");
+
+    echo ("<br>debug=true");
+
+?>
